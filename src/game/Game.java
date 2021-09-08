1d6d3f9aea548c28edb50754a39483eedc542b62
pupil <pupil@ivkhk.ee>
2021-09-08 11:21:47 +0300

    ready game

--- src/game/Game.java
+++ src/game/Game.java
@@ -5,6 +5,7 @@
  */
 package game;
 import java.util.Random;
+import java.util.Scanner;
 
 /**
  *
@@ -16,7 +17,29 @@ public class Game {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+        Random random_gen = new Random();
+        int random_number = random_gen.nextInt(11);//от 0 до 10
+        System.out.println(random_number);
+        Scanner number_scan = new Scanner(System.in);
+        System.out.println("Компьютер загада число. Попробуйте его угадать.");
+        System.out.print("Введите число от 0 до 10: ");
+        int number = number_scan.nextInt();
+        if(number>10 || number<0){ // || это знак or; && это знак and
+            System.out.println("Ваше число меньше 0 или больше 10");
+        }
+        else if (number == random_number){
+            System.out.println("Вы угадали число!");
+        }
+        else if (number != random_number){
+            System.out.printf("Вы не угадали число. Загаданное число: %d%n", random_number);
+             //System.out.println("Вы не угадали число. Загаданное число: "+random_number); //вариант без форматирования
+        }
+        else{
+                
+            System.out.println("Ошибка");}
+            
+            
+            
     }
     
 }
