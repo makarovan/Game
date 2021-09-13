cc2be2df725b68a3e13daa80e71b96549d152279
pupil <pupil@ivkhk.ee>
2021-09-13 09:45:19 +0300

    tri popitki

--- src/game/Game.java
+++ src/game/Game.java
@@ -17,29 +17,33 @@ public class Game {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        int attempt = 0;
         Random random_gen = new Random();
         int random_number = random_gen.nextInt(11);//от 0 до 10
         System.out.println(random_number);
         Scanner number_scan = new Scanner(System.in);
-        System.out.println("Компьютер загада число. Попробуйте его угадать.");
+        System.out.println("Компьютер загада число. Попробуйте его угадать. У вас 3 попытки.");
         System.out.print("Введите число от 0 до 10: ");
-        int number = number_scan.nextInt();
-        if(number>10 || number<0){ // || это знак or; && это знак and
-            System.out.println("Ваше число меньше 0 или больше 10");
+        
+        for (int i=0; i <3; i++){ //три попытки
+            int number = number_scan.nextInt();
+            if(number>10 || number<0){ // || это знак or; && это знак and
+                System.out.println("Ваше число меньше 0 или больше 10");
+            }
+            else if (number == random_number){
+                System.out.println("Вы угадали число!");
+                break;
+            }
+            else{
+                if(attempt<2){
+                    System.out.print("Вы не угадали число. Попробуй еще раз: ");
+                     //System.out.println("Вы не угадали число. Загаданное число: "+random_number); //вариант без форматирования
+                }
+                else{
+                    System.out.println("Ты проиграл");
+                }
+            }
+            attempt++;
         }
-        else if (number == random_number){
-            System.out.println("Вы угадали число!");
-        }
-        else if (number != random_number){
-            System.out.printf("Вы не угадали число. Загаданное число: %d%n", random_number);
-             //System.out.println("Вы не угадали число. Загаданное число: "+random_number); //вариант без форматирования
-        }
-        else{
-                
-            System.out.println("Ошибка");}
-            
-            
-            
     }
-    
 }
